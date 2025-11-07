4bc621cdd834f8b22774301c25f6a0669dd4cd29
Zeeshan <mzeeshi187@gmail.com>
2025-11-08 01:50:31 +0500

    issues fixing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+export const metadata = {
+  metadataBase: new URL('https://thestartswitch.vercel.app'),
+};
 
 export default function RootLayout({
   children,
